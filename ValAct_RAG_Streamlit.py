441dda6fa906ce6bb75d89c3bd4dcc99ab5a5a2f
Dan Kim <Dan153TCIM@gmail.com>
2024-03-02 20:48:46 -0600

    sqlite3

--- ValAct_RAG_Streamlit.py
+++ ValAct_RAG_Streamlit.py
@@ -4,9 +4,10 @@ import streamlit as st
 import os
 
 # sqlite3 related (for Streamlit)
-# import pysqlite3
-# import sys
-# sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+import pysqlite3
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 # Langchain and Vector DB
 from langchain import hub
